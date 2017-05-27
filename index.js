81b1d5624555f55c538bbf74ac5a198ed938621d
shrishail-smartlybuilt <shrishail.sh92@gmail.com>
2017-05-27 13:16:11 +0530

    initial version ready

---
+++ index.js
@@ -0,0 +1,12 @@
+var browserSync = require('browser-sync');
+var colors = require('colors');
+
+var url = process.argv[2];
+
+if (!url) {
+    console.error("No url supplied!!!".red);
+} else {
+    browserSync({
+        proxy: url
+    });
+}
